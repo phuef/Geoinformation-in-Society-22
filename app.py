505c4427e3f8049e5737d7434797b8c1e1d2d93d
Alexander Pilz <alex.pilz95@googlemail.com>
2022-11-23 16:05:59 +0100

    Hello World and Test

---
+++ app.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 23 15:33:03 2022
+
+@author: Alexander Pilz
+"""
+from flask import Flask
+import unittest
+import unittest
+import requests
+
+from werkzeug.serving import WSGIRequestHandler
+from werkzeug.serving import BaseWSGIServer
+WSGIRequestHandler.protocol_version = "HTTP/1.1"
+BaseWSGIServer.protocol_version = "HTTP/1.1"
+
+app = Flask(__name__)
+
+@app.route("/", methods = ['GET'])
+def helloWorld():
+    return "Hello World!", 200
+
+#run application
+if __name__ == '__main__':
+    app.run(port=5000, debug=True, use_reloader=False) 
---
+++ unittest.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 23 15:35:31 2022
+
+@author: Alexander Pilz
+"""
+import unittest
+import requests
+import logging
+import subprocess
+
+class test(unittest.TestCase):
+    
+    def test_helloWorld(self):  
+        response = requests.get('http://localhost:5000/')                 
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'Hello World!')
+        
+#run application
+if __name__ == '__main__':
+    unittest.main()
+
