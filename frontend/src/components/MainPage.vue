ac99265de29b2d1edd05aeac88bb203859f7d4e4
jbalzer12 <61976072+jbalzer12@users.noreply.github.com>
2022-11-28 22:36:19 +0100

    first try

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -1,7 +1,16 @@
 <template>
   <v-container fluid style="height: 100vh">
     <v-row style="height: 100%">
-      <v-col> Here the menu will take place </v-col>
+      <v-col
+        ><v-card>
+          <v-level-drawer v-model="drawer" absolute temporary>
+            <v-divider></v-divider>
+            <v-btn icon @click.stop="mini = !mini">
+              <v-icon>mdi-chevron-left</v-icon>
+            </v-btn>
+          </v-level-drawer>
+        </v-card>
+      </v-col>
       <v-col>
         <div id="mapContainer">
           <MapView />
