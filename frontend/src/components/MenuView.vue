718849709279a7c4700089d1a09beb81be6d6bfa
phuef <phil.hueffer@gmail.com>
2022-11-29 08:36:16 +0100

    add pre-configurations & some comments

--- frontend/src/components/MenuView.vue
+++ frontend/src/components/MenuView.vue
@@ -32,6 +32,12 @@
             ></v-slider>
         </div>
         </v-card-text>
+        <v-divider></v-divider>
+        <v-card-text>
+          <div v-for="configuration in configurations" :key="configuration.name">
+            <v-btn width="100%" @click="activeSliders=configuration.activeSliders, adjustSliders(configuration.activeSliders, configuration.values)">{{configuration.name}}</v-btn>
+          </div>
+        </v-card-text>
     </v-card>
   </div>
 </template>
@@ -41,15 +47,22 @@ export default {
   name: "MapView",
   data(){
       return{
-        sliders: [
+        sliders: [ //All availabe sliders
           {name:"Parks",label:"Distance to parks", value:0, active:true, infoLabel:"Move the slider to remove all areas that have a certain distance to parks"},
           {name:"Water",label:"Distance to areas of water", value:0, active:true, infoLabel:"Move the slider to remove all areas that have a certain distance to areas of water"},
           {name:"Trashcans",label:"Distance to trashcan", value:0, active:true, infoLabel:"Move the slider to remove all areas that have a certain distance to trashcans"}
           ],
-          activeSliders:["Parks", "Water", "Trashcans"]
+          activeSliders:["Parks", "Water", "Trashcans"], //The currently active Sliders
+          configurations:[ //The pre-configurations
+            {name:"Find your Park", activeSliders:["Parks", "Water"], values:[200, 0]},
+            {name:"Find your Trashcan", activeSliders:["Trashcans"], values:[0]}
+          ]
       }
   },
   methods:{
+    /**
+     * Changes the active state of the sliders according to the currently active sliders
+     */
     changeActiveState(){
       for (var h in this.sliders){this.sliders[h].active=false}
       for (var i in this.sliders){
@@ -60,13 +73,42 @@ export default {
         }
       }
     },
+    /**
+     * Removes a layer with a given name
+     */
     removeLayer(name){
       for (var j in this.activeSliders){
         if (this.activeSliders[j]==name){
           this.activeSliders.splice(j, 1)
         }
       }
-    }
+    },
+    /**
+     * Adjusts the shown layers according to a given configuration 
+     */
+    adjustSliders(activeSliders, values){
+      for (var h in this.sliders){this.sliders[h].active=false}
+      for (var i in activeSliders){
+        for (var j in this.sliders){
+        if( this.sliders[j].name==activeSliders[i]){
+          this.sliders[j].active=true
+          this.sliders[j].value=values[i]
+        }
+      }
+      }
+      this.removeNotActiveLayers()
+    },
+    /**
+     * Removes all layers that are currently not active
+     */
+    removeNotActiveLayers(){
+      for (var i in this.activeSliders){
+        for(var j in this.sliders)
+        if (this.activeSliders[i]==this.sliders[j].name && this.sliders[j].active==false){
+          this.activeSliders.splice(i, 1)
+        }
+      }
+    },
   }
   
 }
