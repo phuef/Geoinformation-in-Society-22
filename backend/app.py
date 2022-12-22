3c609c3e55ab9d8d26112a336e78b947f69f4bc2
Alexander Pilz <alex.pilz95@googlemail.com>
2022-12-22 14:07:50 +0100

    changes

--- backend/app.py
+++ backend/app.py
@@ -49,21 +49,21 @@ def tea():
 '''
 @app.route("/apiDescription", methods = ['GET'])
 def api():
-    #try:
-    if(request.args.get('f')=="text/html" or request.args.get('f') == None): 
-        response = render_template('open_api_doc.html') 
-        return response, 200
-    elif(request.args.get('f')=="application/json"):
-        file = open('usr/src/backend/data/open_api_doc.json',) 
-        payload = json.load(file) 
-        file.close() 
-        response = jsonify(payload) 
-        return response, 200
-    else:
-        return "HTTP status code 406: not acceptable", 406 
-    #except(Exception):
-        #print(traceback.format_exc())
-        #return "Internal Server Error", 500
+    try:
+        if(request.args.get('f')=="text/html" or request.args.get('f') == None): 
+            response = render_template('open_api_doc.html') 
+            return response, 200
+        elif(request.args.get('f')=="application/json"):
+            file = open('usr/src/backend/data/open_api_doc.json',) 
+            payload = json.load(file) 
+            file.close() 
+            response = jsonify(payload) 
+            return response, 200
+        else:
+            return "HTTP status code 406: not acceptable", 406 
+    except(Exception):
+        print(traceback.format_exc())
+        return "Internal Server Error", 500
 
 '''
 * Title: 
@@ -71,40 +71,39 @@ def api():
 '''
 @app.route('/request/<requestParams>', methods = ['GET', 'OPTIONS'])
 def raster(requestParams):
-    try:
-        if(request.method == 'GET'): #actual request using GET
-            stack = DistanceStack() #create stack
-            #stack.distanceStackInfo()
-            params = literal_eval(requestParams) #parse parameters
-            for i in params:
-                if(i[1] != None and i[2] != None):
-                    if(i[1] > i[2]):
-                        return "Bad Request", 400
-                elif(i[1] == None and i[2] == None):
+    #try:
+    if(request.method == 'GET'): #actual request using GET
+        stack = DistanceStack() #create stack
+        #stack.distanceStackInfo()
+        params = literal_eval(requestParams) #parse parameters
+        for i in params:
+            if(i[1] != None and i[2] != None):
+                if(i[1] > i[2]):
                     return "Bad Request", 400
-            fileID = stack.filterStack(params) #filter stack
-            
-            geojson = stack.filterResult() #load and filter results
-            
-            response = make_response(geojson) #generate response
-            #add headers
-            response.headers.add("Access-Control-Allow-Origin", "*")
-            response.headers.add("Access-Control-Allow-Headers", "*")
-            response.headers.add("Access-Control-Allow-Methods", "*")
-            response.headers.add("Referrer-Policy", 'no-referrer')
-            
-            return response, 200 #return results
-        elif(request.method == 'OPTIONS'): #preflight request using OPTIONS
-            response = make_response() #generate response
-            #add headers
-            response.headers.add("Access-Control-Allow-Origin", "*")
-            response.headers.add("Access-Control-Allow-Headers", "*")
-            response.headers.add("Access-Control-Allow-Methods", "*")
-            response.headers.add("Referrer-Policy", 'no-referrer')
-            return response, 204 #return preflight response
-    except Exception:
-        print(traceback.format_exc())
-        return "Internal Server Error", 500
+            elif(i[1] == None and i[2] == None):
+                return "Bad Request", 400
+        stack.filterStack(params) #filter stack
+        geojson = stack.filterResult() #load and filter results
+        
+        response = make_response(geojson) #generate response
+        #add headers
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "*")
+        response.headers.add("Access-Control-Allow-Methods", "*")
+        response.headers.add("Referrer-Policy", 'no-referrer')
+        
+        return response, 200 #return results
+    elif(request.method == 'OPTIONS'): #preflight request using OPTIONS
+        response = make_response() #generate response
+        #add headers
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "*")
+        response.headers.add("Access-Control-Allow-Methods", "*")
+        response.headers.add("Referrer-Policy", 'no-referrer')
+        return response, 204 #return preflight response
+    #except Exception:
+        #print(traceback.format_exc())
+        #return "Internal Server Error", 500
 
 #run application
 if __name__ == '__main__':
@@ -112,4 +111,4 @@ if __name__ == '__main__':
     * Port: 5050
     '''
     app.run(port=5050, debug=True, use_reloader=False, host='0.0.0.0') #start app
-    #Request localhost:5050/request/[(0,300,300),(1,200,200)]
+    #Request localhost:5050/request/[(0,None,300),(1,None,200)]
