b85f8fff24070828d6421350b160002b909258f7
jbalzer12 <61976072+jbalzer12@users.noreply.github.com>
2023-01-04 23:16:44 +0100

    only a button change. the fact, that the tour lies on top of the header when scrolling is - as far as I found it - a bug from the used package
    But there exists no serious alternative to this package. So we take the bug as a compromise or we leave the tour out.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="#313641" elevation="0" dark>
+    <v-app-bar z-index="10000" app color="#313641" elevation="0" dark>
       <v-toolbar-title style="cursor: pointer">
         <router-link to="/" style="text-decoration: none; color: inherit">
           Find your spot
